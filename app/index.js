819c921b8689a3da4d71489b2fbc2be9e5e3feb3
saplf <limosaplf@gmail.com>
2022-01-11 00:15:02 +0800

    chore: 添加键盘时间监听

--- app/index.js
+++ app/index.js
@@ -4,9 +4,19 @@ utools.onPluginReady(() => {
 
 function onReady({ code }) {
   utools.setSubInput(onSubInput, '输入游戏名称进行筛选', true);
-  document.getElementById('path').innerText = window.getLibraryFolders();
+  document.onkeydown = onKeyDown;
+
+  document.getElementById('path').innerText = window.getLibraryFoldersPath();
 }
 
 function onSubInput(e) {
   document.getElementById('input').innerText = e.text;
+}
+
+function onKeyDown(e) {
+  const keyMap = {
+    38: '上',
+    40: '下',
+  };
+  document.getElementById('input').innerText = `${keyMap[e.keyCode] || ''}`;
 }
